a16ed75e86ec85539327971116ce83aaab8c9b7a
kaden175ck <61564681+kaden175ck@users.noreply.github.com>
2025-12-13 18:47:53 +0800

    delete discord

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -63,7 +63,7 @@ export const ContactSection = () => {
             </div>
 
             {/* Discord */}
-            <div className="flex items-center gap-4">
+            {/* <div className="flex items-center gap-4">
               <div className="w-12 h-12 rounded-full bg-primary/10 flex items-center justify-center shrink-0">
                 <SiDiscord className="h-6 w-6 text-primary" />
               </div>
@@ -77,7 +77,7 @@ export const ContactSection = () => {
                   Join My Discord
                 </a>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
